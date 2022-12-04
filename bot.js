36123ef80e42dced6dc94d23e20f626e01e7c629
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-12-04 20:48:24 +0400

    feat: slash command features

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require("node:fs")
+const path = require("node:path")
 require('dotenv').config()
 
 const client = new Client({
 	intents: [
-		GatewayIntentBits.Guilds,
+    GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMembers,
@@ -12,18 +14,65 @@ const client = new Client({
     partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
 
-client.on("ready", () => {
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// })
+
+
+// client.on('messageCreate', function (msg) {
+//     if (!msg.author.bot){
+//         if (msg.content === 'zd') {
+//             msg.reply(`zd ${msg.author}`);
+//         }
+//     }
+// });
+client.on('interactionCreate', async interaction => {
     console.log(`Logged in as ${client.user.tag}`)
-})
 
+    if(!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
 
-client.on('messageCreate', function (msg) {
-    if (!msg.author.bot){
-        if (msg.content === 'zd') {
-            msg.reply(`zd ${msg.author}`);
-        }
+    if(!command){
+        console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
     }
-    
+    try{
+        await command.execute(interaction); 
+    }catch (error){
+        console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
+
+client.commands = new Collection()
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    }else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.login(process.env.TOKEN)
